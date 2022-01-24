982fc65eeb80bb2c7b7f4d5fccd2332a899e57c0
siva <sivanandpanda@gmail.com>
2022-01-24 19:14:35 +0800

    added more tests for scoop options

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {});
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
---
+++ sundaes-on-demand/src/pages/entry/ScoopOption.jsx
@@ -0,0 +1,15 @@
+import { Col } from "react-bootstrap";
+
+const ScoopOption = ({ name, imagePath }) => {
+  return (
+    <Col xs={12} s={6} md={4} lg={3} style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} scoop`}
+      />
+    </Col>
+  );
+};
+
+export default ScoopOption;
